f7828f8748cac74dd90d19c0eec3054676bd4f3a
AylaScientist <ayla.bcn@gmail.com>
2024-04-01 14:09:58 +0300

    Logo, fonts, slogan, background image

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,32 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+import GenoIcon from '@/app/icons/genosnp-favicon-white.svg'
+import { actor } from '@/app/ui/fonts';
 
-export default function AcmeLogo() {
+// export default function AcmeLogo() {
+//  return (
+//    <div
+//      className={`${actor.className} flex flex-row items-center leading-none text-white`}
+//    >
+//      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+//      <p className="text-[44px]">Acme</p>
+//    </div>
+//  );
+//}
+
+export default function GenoSNPLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${actor.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <Image
+        priority
+        src = {GenoIcon}
+        height={50}
+        width={50}
+        alt="Follow us on Twitter"
+      />
+      <p className="text-[44px]">GenoSNP</p>
     </div>
   );
 }
